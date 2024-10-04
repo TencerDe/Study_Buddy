6dc1ae855e86a18811a38e57bf04b783ced47a4e
TencerDe <tsharma2907@gmail.com>
2024-10-04 15:27:00 +0530

    allowedhost

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'studybudy.up.railway.app',
+    'localhost:8000'
+]
 
 
 # Application definition
